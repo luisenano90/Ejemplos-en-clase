fed675585f74ea96e9db78566ecc465cd536ba16
Luis Longoria MJ <44417513+luisenano90@users.noreply.github.com>
2020-11-19 21:17:45 -0700

    Add files via upload

---
+++ division entre 0.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+int main()
+{
+	std::cout<<"ingrese un divisor_: ";
+	int x={};
+	std::cin>> x;
+	
+	std:: cout<<"12 /"<<x<< 12 / x << '\n';
+	
+	return 0;
+	
+}
---
+++ porcentaje.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <cmath>
+
+int num_1, num_2, num_3, num_4, resul = 0;
+
+int main ()
+{
+	
+	num_1 = 4;
+	num_2 = 5;
+	num_3 = 6;
+	num_4 = 3;
+	resul = 6 + 5 * 4 % 3;
+	
+	std::cout<<resul<<'\n';
+	
+	return 0;
+}
