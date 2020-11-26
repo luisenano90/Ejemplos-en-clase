ba81367d188c1abcc22e0144b00448dc2745df0a
Luis Longoria MJ <44417513+luisenano90@users.noreply.github.com>
2020-11-25 18:30:22 -0700

    Add files via upload

---
+++ ejemplo 1.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+
+namespace foo
+{
+	int doSomething(int x, int y)
+	{
+		
+		return x + y;
+		
+	}
+}
+
+namespace goo
+{
+	
+	int doSomething(int x, omt y)
+	{
+		return x - y;
+		
+	}
+	
+}
+
+int main()
+{
+	std:: cout foo::doSomething(4,3)<<'\n';
+	return 0;
+	
+}
